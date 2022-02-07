66ca05594833f76b18ed0da3db5f247f6d921621
Daniel Alexandre <liengesbor@gmail.com>
2022-02-06 16:20:57 -0800

    Initial commit

---
+++ src/SkyBoundTechSyliusShippoPlugin.php
@@ -0,0 +1,13 @@
+<?php
+
+declare(strict_types=1);
+
+namespace SkyBoundTech\SyliusShippoPlugin;
+
+use Sylius\Bundle\CoreBundle\Application\SyliusPluginTrait;
+use Symfony\Component\HttpKernel\Bundle\Bundle;
+
+final class SkyBoundTechSyliusShippoPlugin extends Bundle
+{
+    use SyliusPluginTrait;
+}
